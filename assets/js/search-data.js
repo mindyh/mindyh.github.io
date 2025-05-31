68521d57a003673bbd8f347971adbcaccbc9ea77
mindyh <mindyh@users.noreply.github.com>
2025-05-31 21:53:52 +0000

    Deploying to gh-pages from @ mindyh/mindyh.github.io@9c66006e88fbdc3d0aeb9b6be86ce49733047490 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,12 +408,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
+          section: "News",},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
@@ -453,6 +448,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-me210-introduction-to-mechatronics",
+          title: 'ME210: Introduction to Mechatronics',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/me210/";
             },},{
         id: 'social-email',
         title: 'email',
