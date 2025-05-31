4f0e1369a530114c6149f9dbef484511cd1f1b37
mindyh <mindyh@users.noreply.github.com>
2025-05-31 08:19:56 +0000

    Deploying to gh-pages from @ mindyh/mindyh.github.io@f39848dcbbd4d25e41669537c3247c71f1288362 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -465,7 +465,7 @@ ninja.data = [{
         title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://github.com/mindyatwork", "_blank");
+          window.open("https://github.com/mindyh", "_blank");
         },
       },{
         id: 'social-linkedin',
