7f47fbe2d062ef262da303402f7b3db64f002a70
mindyh <mindyh@users.noreply.github.com>
2025-06-07 17:55:40 +0000

    Deploying to gh-pages from @ mindyh/mindyh.github.io@85bb8e6390dbd07bf49603ff5798b10d85ee0677 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -397,7 +397,18 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-2",
+            },},{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
+          description: "",
+          section: "News",},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
